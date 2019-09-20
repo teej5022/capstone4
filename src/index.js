9a58af393167ead1a86b37957010ea4bb4ba66b7
teej5022 <tjschultz5022@gmail.com>
2019-09-20 14:09:59 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import { Navbar } from "./pages/navbar";
+import Lord from "./pages/lord";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+import "./styles.scss";
+
+
+function Cap() {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/About" component={About} />
+                <Route path="/Contact" component={Contact} />
+                <Route path="/Lord" component={Lord} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Cap />, rootElement);
---
+++ src/pages/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export function Navbar() {
+    return (
+        <div className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/Lord">Lords</Link>
+            <Link to="/About">About</Link>
+            <Link to="/Contact">Contact</Link>
+        </div>
+
+    );
+};
+
